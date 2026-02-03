469c1ea6b8933fbd5c5ce427424e591cc1f5b65b
picatout <jd_temp@yahoo.fr>
2026-02-03 10:50:21 -0500

    Recoder et activer les mot I:,I;,ADR-VEC Suprimer du dictionnaire certains mots visible seulement à l'interne

---
+++ bios.asm
@@ -0,0 +1,251 @@
+;-------------------------
+; hardware initialization 
+; and BASIC INPUT/OUTPUT 
+;-------------------------
+
+
+RAMBASE = 0x0000	        ;ram base
+STACK_SIZE = 128 
+DSTACK_SIZE = 128 
+TIB_SIZE= 80 
+STACK   =	RAM_END                 ;R: stack  
+DATSTK  =	RAM_SIZE-DSTACK_SIZE 	;S: stack  
+TIBBASE =       DATSTK-TIB_SIZE         ; transaction input buffer addr. 128 bytes
+
+
+;**********************************************************
+        .area DATA (ABS)
+        .org RAMBASE 
+;**********************************************************
+
+;**********************************************************
+        .area SSEG (ABS) ; STACK
+        .org RAM_SIZE-STACK_SIZE-DSTACK_SIZE-TIB_SIZE
+        .ds STACK_SIZE+DSTACK_SIZE+TIB_SIZE
+;   space for DATSTK,TIB and STACK         
+;**********************************************************
+
+
+;**********************************************************
+        .area CODE
+;**********************************************************
+
+;--------------------------------
+; non handled interrupt reset MCU
+; do nothing 
+;--------------------------------
+NonHandledInterrupt:
+       iret 
+
+;--------------------------------
+; used for milliseconds counter 
+; MS is 16 bits counter 
+;--------------------------------
+Timer4Handler:
+	clr TIM4_SR 
+        ldw x,MS 
+        incw x 
+        ldw MS,x
+        ldw x,CNTDWN 
+        jreq 1$
+        decw x 
+        ldw CNTDWN,x 
+1$:         
+        iret 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; UART intterrupt handler 
+;;; on receive character 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;--------------------------
+; UART receive character
+; in a FIFO buffer 
+; CTRL+X reboot MCU  
+;--------------------------
+UartRxHandler: ; console receive char 
+        btjf UART_SR,#UART_SR_RXNE,5$ 
+	ld a,UART_DR 
+	cp a,#CTRL_X  
+	jrne 2$
+        clr FLASH_IAPSR
+	_swreset 	
+2$:
+	push a 
+	ld a,#RX_QUEUE
+	add a,RX_TAIL 
+	clrw x 
+	ld xl,a 
+	pop a 
+	ld (x),a 
+	ld a,RX_TAIL 
+	inc a 
+	and a,#RX_QUEUE_SIZE-1
+	ld RX_TAIL,a 
+5$:	iret 
+
+;; power up entry points and COLD start data
+reset:
+; clear all RAM
+	ldw X,#RAM_END
+1$:
+	clr (X)
+	decw x 
+        jrne 1$
+; set SEEDX and SEEDY to 1 
+        inc SEEDX+1 
+        inc SEEDY+1          
+
+; initialize stacks 
+        LDW     X,#STACK  ;initialize return stack
+        LDW     SP,X
+        LDW     RP0,X
+        LDW     X,#DATSTK ;initialize data stack
+        LDW     SP0,X
+
+; initialize clock to HSI
+; no divisor 16Mhz 
+clock_init:
+        clr CLK_CKDIVR
+
+; initialize UART, 115200 8N1
+uart_init:
+	bset CLK_PCKENR1,#UART_PCKEN
+; baud rate 115200 Fmaster=16Mhz  16000000/115200=139=0x8b
+	mov UART_BRR2,#0x0b ; must be loaded first
+	mov UART_BRR1,#0x08
+        clr UART_DR
+	mov UART_CR2,#((1<<UART_CR2_TEN)|(1<<UART_CR2_REN)|(1<<UART_CR2_RIEN));
+        call clr_scr
+
+; initialize timer4, used for millisecond interrupt  
+	bset CLK_PCKENR1,#CLK_PCKENR1_TIM4 
+        mov TIM4_PSCR,#7 ; prescale 128  
+	mov TIM4_ARR,#125 ; set for 1msec.
+	mov TIM4_CR1,#((1<<TIM4_CR1_CEN)|(1<<TIM4_CR1_URS))
+	bset TIM4_IER,#TIM4_IER_UIE 
+        rim 
+        jp  COLD   ;default=MN1
+
+
+;;;;;;;;;;;;;;;;;;
+;; Basic UART I/O
+;;;;;;;;;;;;;;;;;;
+
+;----------------------
+; clear terminal screen
+;----------------------
+clr_scr:
+        ld a,#27 
+        call putc 
+        ld a,#'c 
+        jra putc 
+
+;-----------------------
+; cehck if char in queue
+; output:
+;    A    0 no char
+;-----------------------
+qchar: 
+        ld a,RX_HEAD 
+        sub a,RX_TAIL 
+        ret 
+
+;------------------------
+; extract char from queue
+;  output:
+;    A     c  
+;------------------------
+getc:
+        pushw y 
+        _ldaz RX_HEAD 
+1$:     cp a,RX_TAIL 
+        jreq 1$ 
+        clrw y 
+        _ldaz RX_HEAD 
+        add a,#RX_QUEUE 
+        ld yl,a 
+        ld a,(y)
+        push a 
+        _ldaz RX_HEAD 
+        inc a 
+        and a,#RX_QUEUE_SIZE-1 
+        _straz RX_HEAD 
+        pop a
+; uppercase letters 
+        cp a,#'a 
+        jrult 9$ 
+        cp a,#'z+1 
+        jrpl 9$ 
+        and a,#0xDF          
+9$:     popw y 
+        ret 
+
+;---------------------------------
+; send character to UART 
+;  input: 
+;     A     c 
+;---------------------------------
+putc:
+        BTJF UART_SR,#UART_SR_TXE,.  ;loop until tx empty 
+        LD    UART_DR,A   ;send A
+        RET        
+
+;-----------------------------
+; move cursor left 
+; 1 character 
+;-----------------------------
+delback:
+    ld a,#BKSPP 
+    callr putc  
+    ld a,#SPC  
+    callr putc 
+    ld a,#BKSPP 
+    jra putc 
+
+
+;-----------------------------------
+; accept line from terminal 
+; input:
+;    A   max length 
+;    Y   input buffer 
+; output:
+;    A   actual len 
+;----------------------------------
+LEN=1 
+LIMIT=2
+CHAR=3  
+VSIZE=3
+getline:
+    sub sp,#VSIZE 
+    ld (LIMIT,SP),a  
+    clr (LEN,SP)   
+1$:
+    callr getc
+    ld (CHAR,sp),a 
+    cp a,#CRR 
+    jreq 9$ 
+    cp a,#BKSPP  
+    jrne 2$
+    tnz (LEN,SP)
+    jreq 1$ 
+    callr delback 
+    dec (LEN,SP)
+    decw y 
+    jra 1$ 
+2$:     
+    cp a,#SPC  
+    jrmi 1$  ; ignore others control char 
+    ld a,(LEN,SP)
+    cp a,(LIMIT,SP)
+    jreq 1$ 
+    ld a,(CHAR,SP)    
+    callr putc
+    ld (y),a 
+    incw y 
+    inc (LEN,SP)
+    jra 1$
+9$:  
+    ld a,(LEN,SP)
+    addw sp,#VSIZE 
+    ret 
+
--- tinyForth.asm
+++ tinyForth.asm
@@ -30,9 +30,9 @@
 ;--------------------------------------------------------------
 	.module TINYFORTH
          .optsdcc -mstm8
-;	.nlist
+	.nlist
         .include "inc/config.inc"
-;	.list
+	.list
 	.page
 
 ;===============================================================
@@ -107,13 +107,6 @@
 ;*********************************************************
 ;	Assembler constants
 ;*********************************************************
-RAMBASE =	0x0000	        ;ram base
-STACK_SIZE = 128 
-DSTACK_SIZE = 128 
-TIB_SIZE= 128 
-STACK   =	RAM_END                 ;R: stack  
-DATSTK  =	RAM_SIZE-DSTACK_SIZE 	;S: stack  
-TIBBASE =       DATSTK-TIB_SIZE         ; transaction input buffer addr. 128 bytes
 
 ;; Memory allocation
 UPP     =     RAMBASE+6          ; systeme variables base address 
@@ -185,88 +178,11 @@ ERR     =     27      ;error escape
 SPC     =     32      ; space 
 TIC     =     39      ;tick
 CALLL   =     0xCD     ;CALL opcode
-IRET_CODE =   0x80    ; IRET opcode 
+IRET_CODE =   0x82    ; IRET opcode 
 ADDWX   =     0x1C    ; opcode for ADDW X,#word  
 JPIMM   =     0xCC    ; JP addr opcode 
 RET_CODE =    0x81    ; exit subroutine 
 
-;**********************************************************
-        .area DATA (ABS)
-        .org RAMBASE 
-;**********************************************************
-
-;**********************************************************
-        .area SSEG (ABS) ; STACK
-        .org RAM_SIZE-STACK_SIZE-DSTACK_SIZE-TIB_SIZE
-        .ds STACK_SIZE+DSTACK_SIZE+TIB_SIZE
-; space for DATSTK,TIB and STACK         
-;**********************************************************
-
-;**********************************************************
-        .area CODE
-;**********************************************************
-
-; non handled interrupt reset MCU
-; do nothing 
-NonHandledInterrupt:
-       iret 
-
-; used for milliseconds counter 
-; MS is 16 bits counter 
-Timer4Handler:
-	clr TIM4_SR 
-        ldw x,MS 
-        incw x 
-        ldw MS,x
-        ldw x,CNTDWN 
-        jreq 1$
-        decw x 
-        ldw CNTDWN,x 
-1$:         
-        iret 
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; UART intterrupt handler 
-;;; on receive character 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;--------------------------
-; UART receive character
-; in a FIFO buffer 
-; CTRL+X reboot MCU  
-;--------------------------
-UartRxHandler: ; console receive char 
-        btjf UART_SR,#UART_SR_RXNE,5$ 
-	ld a,UART_DR 
-	cp a,#CTRL_X  
-	jrne 2$
-        clr FLASH_IAPSR
-	_swreset 	
-2$:
-	push a 
-	ld a,#RX_QUEUE
-	add a,RX_TAIL 
-	clrw x 
-	ld xl,a 
-	pop a 
-	ld (x),a 
-	ld a,RX_TAIL 
-	inc a 
-	and a,#RX_QUEUE_SIZE-1
-	ld RX_TAIL,a 
-5$:	iret 
-
-;; power up entry points and COLD start data
-reset:
-; clear all RAM
-	ldw X,#RAM_END
-1$:
-	clr (X)
-	decw x 
-        jrne 1$
-; set SEEDX and SEEDY to 1 
-        inc SEEDX+1 
-        inc SEEDY+1          
-	jp ORIG
 
 ; COLD initialize these variables.
 UZERO:
@@ -283,38 +199,6 @@ UZERO:
         .word      LASTN   ;ULAST
         .word      0       ;UOFFSET 
 UEND:   .word      0
-
-ORIG:   
-; initialize stacks 
-        LDW     X,#STACK  ;initialize return stack
-        LDW     SP,X
-        LDW     RP0,X
-        LDW     X,#DATSTK ;initialize data stack
-        LDW     SP0,X
-
-; initialize clock to HSI
-; no divisor 16Mhz 
-clock_init:
-        clr CLK_CKDIVR
-
-; initialize UART, 115200 8N1
-uart_init:
-	bset CLK_PCKENR1,#UART_PCKEN
-; baud rate 115200 Fmaster=16Mhz  16000000/115200=139=0x8b
-	mov UART_BRR2,#0x0b ; must be loaded first
-	mov UART_BRR1,#0x08
-        clr UART_DR
-	mov UART_CR2,#((1<<UART_CR2_TEN)|(1<<UART_CR2_REN)|(1<<UART_CR2_RIEN));
-        call clr_scr
-
-; initialize timer4, used for millisecond interrupt  
-	bset CLK_PCKENR1,#CLK_PCKENR1_TIM4 
-        mov TIM4_PSCR,#7 ; prescale 128  
-	mov TIM4_ARR,#125 ; set for 1msec.
-	mov TIM4_CR1,#((1<<TIM4_CR1_CEN)|(1<<TIM4_CR1_URS))
-	bset TIM4_IER,#TIM4_IER_UIE 
-        rim 
-        jp  COLD   ;default=MN1
 
         LINK = 0  ; used by _HEADER macro 
 
@@ -2264,128 +2148,6 @@ NUMQ6:
         CALL     BASE
         JP       STORE
 
-;;;;;;;;;;;;;;;;;;
-;; Basic UART I/O
-;;;;;;;;;;;;;;;;;;
-
-;----------------------
-; clear terminal screen
-;----------------------
-clr_scr:
-        ld a,#27 
-        call putc 
-        ld a,#'c 
-        jra putc 
-
-;-----------------------
-; cehck if char in queue
-; output:
-;    A    0 no char
-;-----------------------
-qchar: 
-        ld a,RX_HEAD 
-        sub a,RX_TAIL 
-        ret 
-
-;------------------------
-; extract char from queue
-;  output:
-;    A     c  
-;------------------------
-getc:
-        pushw y 
-        _ldaz RX_HEAD 
-1$:     cp a,RX_TAIL 
-        jreq 1$ 
-        clrw y 
-        _ldaz RX_HEAD 
-        add a,#RX_QUEUE 
-        ld yl,a 
-        ld a,(y)
-        push a 
-        _ldaz RX_HEAD 
-        inc a 
-        and a,#RX_QUEUE_SIZE-1 
-        _straz RX_HEAD 
-        pop a
-; uppercase letters 
-        cp a,#'a 
-        jrult 9$ 
-        cp a,#'z+1 
-        jrpl 9$ 
-        and a,#0xDF          
-9$:     popw y 
-        ret 
-
-;---------------------------------
-; send character to UART 
-;  input: 
-;     A     c 
-;---------------------------------
-putc:
-        BTJF UART_SR,#UART_SR_TXE,.  ;loop until tx empty 
-        LD    UART_DR,A   ;send A
-        RET        
-
-;-----------------------------
-; move cursor left 
-; 1 character 
-;-----------------------------
-delback:
-    ld a,#BKSPP 
-    callr putc  
-    ld a,#SPC  
-    callr putc 
-    ld a,#BKSPP 
-    jra putc 
-
-
-;-----------------------------------
-; accept line from terminal 
-; input:
-;    A   max length 
-;    Y   input buffer 
-; output:
-;    A   actual len 
-;----------------------------------
-LEN=1 
-LIMIT=2
-CHAR=3  
-VSIZE=3
-getline:
-    sub sp,#VSIZE 
-    ld (LIMIT,SP),a  
-    clr (LEN,SP)   
-1$:
-    callr getc
-    ld (CHAR,sp),a 
-    cp a,#CRR 
-    jreq 9$ 
-    cp a,#BKSPP  
-    jrne 2$
-    tnz (LEN,SP)
-    jreq 1$ 
-    callr delback 
-    dec (LEN,SP)
-    decw y 
-    jra 1$ 
-2$:     
-    cp a,#SPC  
-    jrmi 1$  ; ignore others control char 
-    ld a,(LEN,SP)
-    cp a,(LIMIT,SP)
-    jreq 1$ 
-    ld a,(CHAR,SP)    
-    callr putc
-    ld (y),a 
-    incw y 
-    inc (LEN,SP)
-    jra 1$
-9$:  
-    ld a,(LEN,SP)
-    addw sp,#VSIZE 
-    ret 
-
 ;;;;;;;;;;;;;;;;;;;;;;
 ; forth terminal 
 ; interface 
@@ -2620,7 +2382,8 @@ DOT1:   CALL     STR
 ;       Scan string delimited by c.
 ;       Return found string and its offset.
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-        _HEADER PARS,5,"PARS$"
+;        _HEADER PARS,5,"PARS$"
+PARS: 
         CALL     TEMP
         CALL     STORE
         CALL     OVER
@@ -2939,7 +2702,8 @@ ABOR2:  CALL     DOSTR
 ;       Interpret a word. If failed,
 ;       try to convert it to an integer.
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-        _HEADER INTER,10,"$INTERPRET"
+;        _HEADER INTER,10,"$INTERPRET"
+INTER: 
         CALL     NAMEQ
         CALL     QDUP    ;?defined
         CALL     QBRAN
@@ -3421,7 +3185,8 @@ STRCQ:
 ;       $"     ( -- ; <string> )
 ;       Compile an inline string literal.
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-        _HEADER STRQ,IMEDD+COMPO+2,'$"'
+;        _HEADER STRQ,IMEDD+COMPO+2,'$"'
+STRQ: 
         CALL     COMPI
         .word STRQP 
         JP     STRCQ
@@ -3506,9 +3271,9 @@ PNAM1:  CALL     STRQP
 ;       $COMPILE        ( a -- )
 ;       Compile next word to
 ;       dictionary as a token or literal.
-; expect IAP unlocked 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-        _HEADER SCOMP,8,"$COMPILE"
+;        _HEADER SCOMP,8,"$COMPILE"
+SCOMP: 
         CALL     NAMEQ
         CALL     QDUP    ;?defined
         CALL     QBRAN
@@ -3552,35 +3317,6 @@ SCOM2:  CALL     NUMBQ   ;try to convert to number
         CALL  UPDATPTR 
         RET 
 
-.if 0 ;**************************
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;       Terminate an ISR definition 
-;       retourn ca of ISR as double
-;       I; ( -- ud )
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-       _HEADER ISEMI,2+IMEDD+COMPO,^/"I;"/
-        subw x,#CELLL  
-        ldw y,#IRET_CODE 
-        ldw (x),y 
-        call CCOMMA
-        call LBRAC 
-        call FMOVE
-        CALL CPP
-        call AT 
-        call SWAPP 
-        CALL CPP 
-        call STORE 
-        call UPDATPTR 
-        call EEPVP 
-        _DROP 
-        call AT 
-        call VPP 
-        call STORE 
-        JP ZERO 
-
- .ENDIF ; ************************          
-        
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;       ]       ( -- )
 ;       Start compiling words in
@@ -3596,8 +3332,8 @@ SCOM2:  CALL     NUMBQ   ;try to convert to number
 ;       CALL,    ( ca -- )
 ;       Compile a subroutine call.
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-        _HEADER JSRC,5,^/"CALL,"/
-;JSRC: 
+;        _HEADER JSRC,5,^/"CALL,"/
+JSRC: 
         LDW Y,#DROP 
         LDW YTEMP,Y 
         LDW Y,X 
@@ -3638,18 +3374,63 @@ JSRC2:
         CALL   SNAME
         JP     RBRAC
 
-.IF 0 ;************************
+.IF 1 ;************************
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;  interrupt routines 
+;  words 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;       I:  ( -- )
-;       Start interrupt service 
-;       routine definition
-;       those definition have 
-;       no name.
+;       I:  ( -- ca )
+; Start interrupt service 
+; routine definition
+; those definition have 
+; no name.
+; return interrput code address 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
         _HEADER ICOLON,2,"I:"
-        call INITOFS 
-        jp RBRAC  
+        SUBW    X,#CELLL 
+        LDW     Y,UCP 
+        LDW     (X),Y  ; ca of interrupt 
+        JP      RBRAC  
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;       I; ( ca -- int-vec )
+;  Terminate an ISR definition 
+;  return interrupt vector as double 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+       _HEADER ISEMI,2+IMEDD+COMPO,^/"I;"/
+        _DOLIT  IRET_CODE  
+        CALL    CCOMMA
+        CALL    CPP
+        CALL    AT 
+        CALL    EEPCP
+        CALL    FSTOR
+        SUBW    X,#CELLL  
+        LDW     Y,#IRET_CODE<<8 
+        ldw     (X),Y
+        JP      LBRAC 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;  ADR-VEC ( n -- adr )
+;  return address of interrupt 
+;  vector 'n' 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+        _HEADER ADRVEC,7,"ADR-VEC"
+        LDW     Y,X
+        LDW     Y,(Y)
+        CPW     Y,#29  
+        JRUGT   4$
+        LD      A,#4 
+        MUL     Y,A  
+        ADDW    Y,#FLASH_BASE
+        LDW     (X),Y 
+        RET 
+4$:      
+       CALL     ABORQ
+       .byte    10
+       .ascii " bad vector"
 
 .ENDIF ;*************************
 
@@ -3799,7 +3580,8 @@ DO_DCONST:
 ;       Display a string. Filter
 ;       non-printing characters.
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-        _HEADER UTYPE,5,"_TYPE"
+;        _HEADER UTYPE,5,"_TYPE"
+UTYPE: 
         CALL     TOR     ;start count down loop
         JRA     UTYP2   ;skip first pass
 UTYP1:  CALL     DUPP
@@ -3816,7 +3598,8 @@ UTYP2:  CALL     DONXT
 ;       Dump u bytes from ,
 ;       leaving a+u on  stack.
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-        _HEADER DUMPP,3,"DM+"
+;        _HEADER DUMPP,3,"DM+"
+DUMPP: 
         CALL     OVER
         CALL     DOLIT
         .word      4
@@ -4020,7 +3803,8 @@ PRINT_VERSION:
 ;       'BOOT   ( -- a )
 ;       The application startup vector.
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-        _HEADER TBOOT,5,"'BOOT"
+;        _HEADER TBOOT,5,"'BOOT"
+TBOOT:
         SUBW    X,#CELLL 
         LDW     Y,EEP_RUN
         JRNE    1$
